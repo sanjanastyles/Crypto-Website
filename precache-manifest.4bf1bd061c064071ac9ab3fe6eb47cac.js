102f261dcdaa42b9c515e00b1dc9dd6d400cef89
KANATA <sanjana2842002@gmail.com>
2022-08-13 21:19:55 +0530

    Updates

---
+++ precache-manifest.4bf1bd061c064071ac9ab3fe6eb47cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0296f45ae646862c049",
+    "url": "/Crypto-Website/static/css/main.0845a0a3.chunk.css"
+  },
+  {
+    "revision": "a0296f45ae646862c049",
+    "url": "/Crypto-Website/static/js/main.a0296f45.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/Crypto-Website/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "3a328a21044fa377a2ad",
+    "url": "/Crypto-Website/static/js/2.3a328a21.chunk.js"
+  },
+  {
+    "revision": "5efaf212eb04e6f63908",
+    "url": "/Crypto-Website/static/js/runtime~main.5efaf212.js"
+  },
+  {
+    "revision": "ad7456934c52e38eedfc8e0d9059d5ab",
+    "url": "/Crypto-Website/index.html"
+  }
+];
